82ef1e865aca155fff9c18e9ecab14d9f711a0ed
Людмила Трибушинина <shilova_lu@mail.ru>
2023-04-24 20:10:17 +0400

    Added idling in code

--- app/src/main/java/ru/kkuzmichev/simpleappforespresso/ui/gallery/GalleryFragment.java
+++ app/src/main/java/ru/kkuzmichev/simpleappforespresso/ui/gallery/GalleryFragment.java
@@ -17,6 +17,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
+import ru.kkuzmichev.simpleappforespresso.EspressoIdlingResources;
 import ru.kkuzmichev.simpleappforespresso.R;
 import ru.kkuzmichev.simpleappforespresso.databinding.FragmentGalleryBinding;
 
@@ -59,6 +60,7 @@ public class GalleryFragment extends Fragment {
 
 
     private void fakeLoadData() {
+        EspressoIdlingResources.increment();
         progressBar.setVisibility(View.VISIBLE);
         recyclerView.setVisibility(View.INVISIBLE);
         Handler handler = new Handler();
@@ -68,6 +70,7 @@ public class GalleryFragment extends Fragment {
                 {
                     progressBar.setVisibility(View.INVISIBLE);
                     recyclerView.setVisibility(View.VISIBLE);
+                    EspressoIdlingResources.decrement();
                 }
             }
         }, 1500);
